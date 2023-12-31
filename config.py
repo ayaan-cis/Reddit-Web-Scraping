13c7fddab53407fe9523b2158da45e07ac90ca99
Ayaan Syed <114111679+ayaan-cis@users.noreply.github.com>
2023-12-30 20:48:43 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import praw
+
+reddit_read_only = praw.Reddit(client_id="#", client_secret="#", user_agent="#")
+
+# Authorized instance
+reddit_authorized = praw.Reddit(client_id="#", client_secret="#", user_agent="#", username="#", password="#")
+
